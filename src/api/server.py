5a4f5763ba9923fe7d67f6d3aa9d6b8f1ea14393
n-shinde <nashinde@calpoly.edu>
2023-10-30 13:48:49 -0700

    Version 1 - added server.py

---
+++ src/api/server.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, exceptions
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+from src.api import routes
+import json
+import logging
+import sys
+from starlette.middleware.cors import CORSMiddleware
+
+
+description = """
+Find the best routes your friends love on PeakPeeps!
+"""
+
+app = FastAPI(
+    title="PeakPeeps",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Robin Tun",
+        "email": "rotun@calpoly.edu",
+    },
+)
+
+# origins = ["https://potion-exchange.vercel.app"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "OPTIONS"],
+#     allow_headers=["*"],
+# )
+
+
+app.include_router(routes.router)
+
+@app.exception_handler(exceptions.RequestValidationError)
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request, exc):
+    logging.error(f"The client sent invalid data!: {exc}")
+    exc_json = json.loads(exc.json())
+    response = {"message": [], "data": None}
+    for error in exc_json:
+        response['message'].append(f"{error['loc']}: {error['msg']}")
+
+    return JSONResponse(response, status_code=422)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to PeakPeeps"}

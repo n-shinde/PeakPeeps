6700028266cca2f86ba7402835c0f7165607c9aa
Josh <jqueja@calpoly.edu>
2023-11-07 09:01:56 -0800

    added the api endpoints, checking if it will add them

--- src/database.py
+++ src/database.py
@@ -2,9 +2,13 @@ import os
 import dotenv
 from sqlalchemy import create_engine
 
+import sqlalchemy
+from src import database as db
 def database_connection_url():
     dotenv.load_dotenv()
 
     return os.environ.get("POSTGRES_URI")
+
+print(os.environ.get("POSTGRES_URI"))
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)

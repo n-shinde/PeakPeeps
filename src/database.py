1d03b508b5dfa35c7d4a0c09904630461b99ae36
AlexBuchko <abuchko@calpoly.edu>
2023-11-27 13:59:26 -0800

    Fixing unrepeatable read issue with purchase_coupon

--- src/database.py
+++ src/database.py
@@ -4,10 +4,15 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from src import database as db
 
+
 def database_connection_url():
     dotenv.load_dotenv()
     return os.environ.get("POSTGRES_URI")
 
+
 print(os.environ.get("POSTGRES_URI"))
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+read_repeatable_engine = create_engine(
+    database_connection_url(), pool_pre_ping=True, isolation_level="REPEATABLE READ"
+)

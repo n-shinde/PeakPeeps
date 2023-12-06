b42c52dd5e0048b900139016a49edb16694b39e3
juliaf451 <97061487+juliaf451@users.noreply.github.com>
2023-12-06 10:42:31 -0800

    fixing similar route names catch

--- src/api/routes.py
+++ src/api/routes.py
@@ -229,9 +229,8 @@ def post_add_route(route_to_add: Route):
                 """
                 SELECT 1
                 FROM routes
-                WHERE added_by_user_id != :user_id
-                AND routes.name ILIKE :route_name
-                AND routes.city ILIKE :route_city
+                WHERE routes.name ILIKE '%' || :route_name || '%'
+                AND routes.city ILIKE '%' || :route_city || '%'
                 """
             ),
             {"route_name": route_to_add.name, "route_city": route_to_add.city, "user_id": user_id},
